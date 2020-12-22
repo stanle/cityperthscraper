adb775b32516fb20c0b6c7eb899724c4f343eb38
Daniel Caldeweyher <daniel@nexu.com.au>
2020-12-23 09:02:18 +1000

    headless

--- scraper.py
+++ scraper.py
@@ -42,7 +42,7 @@ def clean_description(description: str) -> str:
 # chrome_options.add_argument('--disable-dev-shm-usage')
 # chrome_options.add_argument('--no-sandbox')
 
-with Browser('chrome', executable_path='/usr/local/bin/chromedriver') as browser:
+with Browser('chrome', executable_path='/usr/local/bin/chromedriver', headless=True) as browser:
 # with Browser('chrome', options=chrome_options) as browser:
     browser.visit(URL)
     links = browser.find_by_css(".list-item > a")
